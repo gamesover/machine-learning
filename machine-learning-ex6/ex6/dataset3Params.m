4cfbe7876d468dbfc79025295b24045976cf99f3
Ken Ding <ken.ding@mail.com>
2019-05-13 16:38:04 +0930

    finish ex6

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,9 +22,26 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+%
+possible_values_C = [0.01 0.03 0.1 0.3 1 3 10 30];
+possible_values_sigma = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+pred_error = 1000;
 
-
+for Ci=1:length(possible_values_C),
+    local_C = possible_values_C(Ci);
+    for sigmai=1:length(possible_values_sigma),
+        local_sigma = possible_values_sigma(sigmai);
+        model = svmTrain(X, y, local_C, @(x1, x2) gaussianKernel(x1, x2, local_sigma));
+        predictions = svmPredict(model, Xval);
+        local_pred_error = mean(double(predictions ~= yval));
+        if local_pred_error < pred_error,
+            pred_error = local_pred_error;
+            C = local_C;
+            sigma = local_sigma;
+        end
+    end
+end
 
 
 
