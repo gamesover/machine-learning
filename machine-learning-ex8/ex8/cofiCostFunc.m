7a41808c3ad5ce3a46e5bc8fc35e026f1a22a4dc
Ken Ding <ken.ding@mail.com>
2019-05-15 01:25:25 +0930

    finish ex8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -44,7 +44,14 @@ Theta_grad = zeros(size(Theta));
 
 
 
+D = (X * Theta' - Y) .* R;
 
+% Normally sum(X(:)), but that looks weird here.
+J = 1/2 * sum(sum(D .^ 2)) + lambda / 2 * sum(sum(Theta .^ 2)) ...
+                           + lambda / 2 * sum(sum(X .^ 2));
+
+X_grad = D * Theta + lambda * X;
+Theta_grad = D' * X + lambda * Theta;
 
 
 
